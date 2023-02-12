458d20b048ab2ef052584a3882551955c772b0ed
dsgreve <dgreve@altair.com>
2023-02-12 09:03:42 -0500

    initial file update

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+import samplefishes from "../sample-fishes";
+import Fish from "./Fish";
+
+class App extends React.Component {
+  // set initial state - load component with empty state
+  state = {
+    fishes: {},
+    order: {}
+  };
+  // lifecycle events
+  addFish = (fish) => {
+    // updating state in react
+    // 1. Take a copy of existing state
+    const fishes = { ...this.state.fishes };
+    // 2.  Add new fish to copy
+    fishes[`fish${Date.now()}`] = fish;
+    //3. set new fishes object to state
+    this.setState({
+      // fishes: fishes
+      fishes
+    });
+  }
+
+  loadSampleFishes = () => {
+    this.setState({fishes: samplefishes});
+  };
+
+  addToOrder = (key) => {
+    // 1. take copy of state
+    const order = {...this.state.order}
+    // 2. add or update order
+    order[key]= order[key] + 1 || 1;
+    // 3 call setstate to update
+    this.setState({ order });
+  }
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish 
+                key={key}
+                index={key}
+                details={this.state.fishes[key]} 
+                addToOrder={this.addToOrder}
+              />
+          ))}
+          </ul>
+        </div>
+        <Order />
+        <Inventory 
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { getFunName } from "../helpers";
+
+// every component is its own class, and needs render in it
+class StorePicker extends React.Component {
+  myInput = React.createRef();
+  
+  goToStore = (event) => {
+    // stop form from submitting
+    event.preventDefault();
+    // get text from input
+    const storeName = this.myInput.current.value;
+    // ref allows us to reference a dom node on page
+    // change page to /store/whatever-they-enter
+    this.props.history.push(`/store/${storeName}`);
+    
+  };
+
+  render() {
+   return (
+    <React.Fragment>
+    <h2>Fish</h2>
+      <form className="store-selector" onSubmit={this.goToStore}>
+        { /** Comment in reactusing block element */  }
+        <h2>Please Enter A Store</h2>
+
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store -></button>
+      </form>
+    </React.Fragment>
+    
+   )
+  }
+}
+
+export default StorePicker;
